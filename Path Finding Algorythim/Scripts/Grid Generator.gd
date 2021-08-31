483bf90063a5c73c1a81160d09c87b34f8e9502b
Megatt4 <pburitysilva@gmail.com>
2021-08-31 19:54:17 -0300

    added Main Project

---
+++ Path Finding Algorythim/Scripts/Grid Generator.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+onready var GridSizeUI_Script = load("res://Scripts/Grid_Size_UI.gd")
+
+
+var ValidGridSize = false
+
+var NumForPerfectGrid = 20
+
+#Size Of the grid (adjustable depending on GridSize var)
+var SizeX = Vector2(100,100)
+var SizeY = Vector2(100,100)
+
+#Size of grid in a 1x1 basis ( User Input )
+var GridSize = 8
+
+func JustDraw(intialposX, intialposY, LineColor, SpacingX, SpacingY):
+	var OGposY = intialposY
+	var OGposX = intialposX
+	var intialposEndX = Vector2(100, 100)
+	var intialposEndY = Vector2(100, 100)
+	SizeX = Vector2(intialposEndY)
+	SizeY = Vector2(intialposEndX)
+
+	for i in range(GridSize):
+		intialposX = intialposX + Vector2(SpacingX)
+
+	for i in range(GridSize):
+		intialposY = intialposY + Vector2(SpacingY)
+
+	intialposEndX = intialposX
+	intialposEndY = intialposY
+
+	for i in range(GridSize + 1):
+		draw_line(OGposY, intialposEndY, LineColor)
+		OGposY = OGposY + Vector2(SpacingX)
+		intialposEndY = intialposEndY + Vector2(SpacingX)
+	
+	for i in range(GridSize + 1):
+		draw_line(OGposX, intialposEndX, LineColor)
+		OGposX = OGposX + Vector2(SpacingY)
+		intialposEndX = intialposEndX + Vector2(SpacingY)
+
+func _draw():
+	JustDraw(Vector2(0, 0), Vector2(0, 0), Color(1.0, 0.0, 0.0), Vector2(0, 30), Vector2(30, 0))
+
---
+++ Path Finding Algorythim/Scripts/Grid_Size_UI.gd
@@ -0,0 +1,19 @@
+extends LineEdit
+
+var GridSize_Range = range(1,99)
+
+var NT = 1
+
+onready var GridGenerator_Script = load("res://Scripts/Grid_Size_UI.gd")
+
+func _on_LineEdit_text_entered(new_text):
+	if new_text.is_valid_integer():
+			NT = str(new_text)
+			for i in GridSize_Range:
+				var i2 = str(i)
+				if NT == i2:
+					GridGenerator_Script.ValidGridSize = true
+					self.clear()
+	else:
+		GridGenerator_Script.ValidGridSize = false
+		self.clear()
